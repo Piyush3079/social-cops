fbcbd18ca06431c76b934c892c40e31b65335643
Piyush Vijay <piyushvijay.1997@gmail.com>
2018-03-23 22:47:13 +0530

    download image functions

--- controller/download-file.js
+++ controller/download-file.js
@@ -1,10 +1,22 @@
 import fs from 'fs';
 import request from 'request';
+import sharp from 'sharp';
 
 const downloadFile = (req, res) => {
-	const { uri } = req.body;
-	request(uri).pipe(fs.createWriteStream('google.jpg')).on('close', () => {
-		console.log('done');
+  const { uri } = req.body;
+  const arr = uri.split('.');
+  const format = arr[arr.length-1];
+  const name = new Date().getTime();
+  request(uri).pipe(fs.createWriteStream(`./public/images/temp/${name}.${format}`)).on('close', () => {
+    sharp(`./public/images/temp/${name}.${format}`)
+      .resize(50, 50)
+      .toFile(`./public/images/temp_compressed/${name}.${format}`, (err) => {
+        if (err) throw err;
+        fs.unlink(`./public/images/temp/${name}.${format}`, (err) => {
+          if (err) throw err;
+          res.download(`./public/images/temp_compressed/${name}.${format}`);
+        });
+      });
 	});
 }
 
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,11 @@
 import getToken from '../controller/get-token';
 import showIndex from '../controller/show-index';
+import downloadFile from '../controller/download-file';
 
 export default (app) => {
   app.get('/', showIndex);
 
   app.post('/get_token', getToken);
+
+  app.post('/download_file', downloadFile);
 };
